d01cd4a6a89ae6b6183b1cf46b23001839df9ee5
z0tedd <z0tedd@gmail.com>
2024-03-30 13:06:20 +0000

    Upd

--- main_file.c/main_file.c.ino
+++ main_file.c/main_file.c.ino
@@ -3,36 +3,69 @@
  * DONE:
  *  1)Basic device assembly
  *  2)BASIC BLUETOOTH DATA EXTRACTION 19.03.2024
+ *  3)BASIC ACCELERATION PROCESSING
+ *  4)BASIC MATRIX SETTING
  * TODO-LIST:
- *  1)BASIC MATRIX SETTING
- *  2)BASIC ACCELERATION PROCESSING
- *  3)BASIC PULSE PROCESSING
- *  4)BASIC EEPROM SETTINGS EXTRACTION
- *  5)ACYNSRONIC FUNCTIONS PROCESSING WITH INTERAPTIONS
- *  6)ADVANCING ALGORITHMS DEVELOPMENT
- *  7)ADVANCED APP 
+ *  1)BASIC PULSE PROCESSING
+ *  2)BASIC EEPROM SETTINGS EXTRACTION
+ *  3)ACYNSRONIC FUNCTIONS PROCESSING WITH INTERAPTIONS
+ *  4)ADVANCING ALGORITHMS DEVELOPMENT
+ *  5)ADVANCED APP 
  */
 
 //Headers
+#include <Wire.h> // I2C library
+#include <TroykaIMU.h> // Accelerometer library
 #include <SoftwareSerial.h>
-#include <Adafruit_NeoPixel.h> // библиотека для работы с RGB-матрицей
-#include <EEPROM.h> // Доступ к энергозависимой памяти
+#include <Adafruit_NeoPixel.h> // Matrix library
+#include <EEPROM.h> //Library for non-volatile memory  
 
 //Macros
-#define MATRIX_PIN    11 // номер пина, к которому подключена RGB-матрица
+#define MATRIX_PIN    D4 // номер пина, к которому подключена RGB-матрица
 #define LED_COUNT 16 // количество светодиодов в матрице
-#define BTXPin D8
-#define BRXPin D7
+#define BTXPin D8 //Bluetooth Software serial tx pin
+#define BRXPin D7 //Bluetooth Software serial rx pin
 #define Board_LED D13
 
 //Header's objects
 SoftwareSerial HM10(BTXPin, BRXPin);
-//Adafruit_NeoPixel matrix(LED_COUNT, MATRIX_PIN, NEO_GRB + NEO_KHZ800); //Adafruit neopixel object
+Accelerometer Accel; //Accelerometer pins SDA(D14) - D  SCL(D15) - C 
+Adafruit_NeoPixel Matrix(LED_COUNT, MATRIX_PIN, NEO_GRB + NEO_KHZ800); //Adafruit neopixel object
 
 //Classic objects
-String startSetting;//Data from phone(time synchronization)
+String startSetting; //Data from phone(time synchronization)
+float Ax, Ay, Az; // Acceleration divided along the axes
+//uint32_t startColor = Matrix.ColorHSV(60,127,255); //https://findhow.org/5060-konverter-tsvetov.html HSVColor(60, 100%, 50%).
+uint32_t startColor = Matrix.Color(255,255,0);
 
-void MatrixDraw();
+void lightWaking(){
+  unsigned long startTime = millis();
+  int i = 0;
+  while (i<=255){
+    if(millis() - startTime > 100){
+      Matrix.fill(Matrix.Color(255,255,i));
+      //Matrix.fill(Matrix.ColorHSV(60,127+i,255));
+      Matrix.show();
+      i+=15;
+      startTime = millis();
+    }
+  }
+}
+void lightWakingOff(){
+  unsigned long startTime = millis();
+  int i = 255;
+  while (i>=0){
+    if(millis() - startTime > 100){
+      
+      Matrix.fill(Matrix.Color(255,255,i));
+      //Matrix.fill(Matrix.ColorHSV(60,127+i,255));
+      Matrix.show();
+      i-=15;
+      startTime = millis();
+    }
+  }
+}
+
 String readData(){
   String output = "";
   HM10.listen();
@@ -46,43 +79,32 @@ String readData(){
 void setup() {
   Serial.begin(9600);
   HM10.begin(9600);
+  Accel.begin();
+  Accel.setRange(AccelerometerRange::RANGE_2G);
+  Serial.println("Initialization completed");
   Serial.println("HM10 serial started at 9600");
-  // put your setup code here, to run once:
-  //matrix.begin(); // Matrix initialization
+  Matrix.begin(); // Matrix initialization
+  Matrix.setBrightness(50); //Brightness basic setting
+  Matrix.show(); // Initialize all pixels to 'off'
+  
   //Reading until device gets data, exceptions - AT commands
+  /*
   do{startSetting = readData();}
   while(startSetting == ""||startSetting == "OK+LOST"
   ||startSetting == "OK+CONN"); 
+  */
 }
-
 
 void loop(){
-  Serial.println(startSetting);
-  
+  Accel.readAccelerationAXYZ(Ax, Ay, Az);
+  startSetting = readData();
+  Serial.print(Ax);
+  Serial.print("\t\t");
+  Serial.print(Ay);
+  Serial.print("\t\t");
+  Serial.print(Az);
+  Serial.println("");
+  lightWaking();
+  lightWakingOff();
+  if (startSetting!="")Serial.println(startSetting);
 }
-/*
-void loop()
-{
-      // заполняем RGB-матрицу по сегментам «бегущий огонь» красного цвета
-      colorWipe(matrix.Color(255, 0, 0), 50);
-      // заполняем RGB-матрицу по сегментам «бегущий огонь» зелёного цвета
-      colorWipe(matrix.Color(0, 255, 0), 50);
-      // заполняем RGB-матрицу по сегментам «бегущий огонь» синего цвета
-      colorWipe(matrix.Color(0, 0, 255), 50);
-      // гасим RGB-матрицу по сегментам «бегущая тень»
-      colorWipe(matrix.Color(0, 0, 0), 50);
-}
-     
-// функция заполнения каждого сегмента
-void colorWipe(uint32_t c, uint8_t wait)
-{
-  for (uint16_t i = 0; i < matrix.numPixels(); i++) {
-    // заполняем текущий сегмент выбранным цветом
-    matrix.setPixelColor(i, c);
-    matrix.show();
-    // ждём
-    delay(wait);
-  }
-}
-*/
- //*/
